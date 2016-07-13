410372d4ec4b41b0e31a188590c1ab868799de29
Stan Chang Khin Boon <khinboon@gmail.com>
2016-07-13 23:24:55 +0800

    Adds hidden_textfield plugin. (selective/selectize.js#110)

--- dist/js/selectize.js
+++ dist/js/selectize.js
@@ -2889,6 +2889,24 @@
 	
 	});
 	
+	// https://github.com/selectize/selectize.js/issues/110#issuecomment-167840205
+	Selectize.define('hidden_textfield', function(options) {
+		var self = this;
+		this.showInput = function() {
+			this.$control.css({cursor: 'pointer'});
+			this.$control_input.css({opacity: 0, position: 'relative', left: self.rtl ? 10000 : -10000 });
+			this.isInputHidden = false;
+		};
+	
+		this.setup_original = this.setup;
+	
+		this.setup = function() {
+			self.setup_original();
+			this.$control_input.prop("disabled","disabled");
+		}
+	});
+	
+	
 	Selectize.define('optgroup_columns', function(options) {
 		var self = this;
 	
